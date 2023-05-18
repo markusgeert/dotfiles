46582eacfbec78e4a92225e5b6c3f4ac15bbf820
Max Stevens <maxstevens2708@gmail.com>
2023-05-18 23:56:24 +0200

    Add more custom devicon configs

--- .config/nvim/after/plugin/nvim-web-devicons.lua
+++ .config/nvim/after/plugin/nvim-web-devicons.lua
@@ -1,16 +1,49 @@
 require("nvim-web-devicons").setup {
     strict = true,
 
+    override_by_extension = {
+        ["code\\-workspace"] = {
+            icon = "󰨞",
+            color = "#0098FF",
+            name = "CodeWorkspace"
+        },
+        ["ts"] = {
+            color = "#519aba",
+            name = "Typescript",
+            icon = "󰛦"
+        },
+        ["d.ts"] = {
+            color = "#519aba",
+            name = "TypescriptTypes",
+            icon = ""
+        },
+    },
+
     override_by_filename = {
         [".firebaserc"] = {
             icon = "󰥧",
             color = "#FFCA28",
             name = "Firebaserc"
         },
-        -- ["package.json"] = {
-        --     icon = "󰎙",
-        --     color = "#44883e",
-        --     name = "Nodejs"
-        -- }
+        [".gitignore"] = {
+            icon = "󰊢",
+            color = "#f14e32",
+            name = "Gitignore"
+        },
+        [".nvmrc"] = {
+            icon = "󰎙",
+            color = "#44883e",
+            name = "Nvmrc"
+        },
+        ["package.json"] = {
+            icon = "󰎙",
+            color = "#44883e",
+            name = "Package"
+        },
+        ["package-lock.json"] = {
+            icon = "󰎙",
+            color = "#44883e",
+            name = "PackageLock"
+        }
     }
 }
