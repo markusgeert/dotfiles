9f89608027a627a87cbf10c9b1f633e93bf5c7f5
Max Stevens <maxstevens2708@gmail.com>
2023-05-19 02:00:50 +0200

    Add gitignore which includes all files in the nvim dir

---
+++ .config/nvim/after/plugin/help.lua
@@ -0,0 +1,7 @@
+vim.api.nvim_create_autocmd("BufWinEnter", {
+    group = vim.api.nvim_create_augroup("help_window_right", {}),
+    pattern = { "*.txt" },
+    callback = function()
+        if vim.o.filetype == 'help' then vim.cmd.wincmd("L") end
+    end
+})
