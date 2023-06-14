3b7cda63c58796c1aadf2ab2cec9c58fe1315f86
Max Stevens <maxstevens2708@gmail.com>
2023-06-14 09:28:13 +0200

    Add light theme in packer

--- .config/nvim/lua/max/packer.lua
+++ .config/nvim/lua/max/packer.lua
@@ -9,12 +9,12 @@ return require('packer').startup(function(use)
 
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        -- or                            , branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
     use 'Mofiqul/vscode.nvim'
     use 'tanvirtin/monokai.nvim'
+    use { "catppuccin/nvim", as = "catppuccin" }
 
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use('nvim-treesitter/playground')
