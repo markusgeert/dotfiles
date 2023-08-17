1b505182cd779596b996d67009947ca1895b543e
Max Stevens <maxstevens2708@gmail.com>
2023-08-17 12:17:02 +0200

    Move files to thrash with 'D' in netrw

--- .config/nvim/after/plugin/netrw.lua
+++ .config/nvim/after/plugin/netrw.lua
@@ -2,5 +2,14 @@ require("netrw").setup {
     icons = {
         file = ""
     },
-    use_devicons = true
+    use_devicons = true,
+    mappings = {
+        ['D'] = function(payload)
+            local filepath = payload["dir"] .. "/" .. payload["node"]
+
+            vim.fn.system("trash " .. filepath)
+
+            vim.cmd("e!")
+        end
+    }
 }
