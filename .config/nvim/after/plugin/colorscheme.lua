131435d28d3db0240010c76c4c0acc7456ef3676
Max Stevens <maxstevens2708@gmail.com>
2024-02-15 10:57:55 +0100

    Set colorschema in one file

---
+++ .config/nvim/after/plugin/colorscheme.lua
@@ -0,0 +1,19 @@
+-- local palette = monokai.classic
+-- local monokai = require('monokai')
+--
+-- monokai.setup {
+--     custom_hlgroups = {
+--         ['@include'] = {
+--             fg = palette.pink
+--         },
+--         ['Comment'] = {
+--             fg = palette.base6
+--         }
+--     },
+-- }
+--
+-- require("catppuccin").setup({
+--     flavour = "latte"
+-- })
+
+require("onedark").setup()
