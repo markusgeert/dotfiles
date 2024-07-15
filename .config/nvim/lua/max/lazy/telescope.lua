a276beae211081d3371f9a43648f962814eb632e
Max Stevens <maxstevens2708@gmail.com>
2024-07-15 20:19:49 +0200

    feat: enable regex in telescope grep

--- .config/nvim/lua/max/lazy/telescope.lua
+++ .config/nvim/lua/max/lazy/telescope.lua
@@ -17,7 +17,7 @@ return {
         end)
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") });
+            builtin.grep_string({ search = vim.fn.input("Grep > "), use_regex = true, });
         end)
     end
 }
